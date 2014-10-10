ce6dad32e84c04e418c1d0430f208d7dfa319ab8
Caroline Visoto Nunes <carolvisoto@gmail.com>
2014-10-10 10:39:39 +0200

    fixed radio button and animal parameter problem in questions.js

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -14,31 +14,29 @@ function hideContainer() {
     $("#container").hide();
 }
 function showQuiz() {
-
    $('#start').click(function(){
        showDivQuiz();
        hideContainer();
 
        $.getJSON("/questions/rand", function(quiz) {  
-         question(quiz)
+         question(quiz);
        });
    });
    showAnswer();
 }
 
 function showAnswer() {
-
    $('#send').click(function(){
         hideContainer();
         var option = $('input[type="radio"]:checked').val();
-        numberOfQuestions +=1
-       $.getJSON("/questions/rand", function(quiz) { 
+        var animal = $('#animal').val();
+        numberOfQuestions += 1;
+        optionChosen(option, animal);
+        $.getJSON("/questions/rand", function(quiz) { 
           if (numberOfQuestions < MAX_POSSIBILITIES){
               showDivQuiz();
               question(quiz);
-              optionChosen(option, quiz.animal)
-              
-          }else{
+          } else {
                hideDivQuiz();
                message();
           }  
@@ -48,35 +46,34 @@ function showAnswer() {
    
 }
 function optionChosen(option, animal){
-  console.log("func optionChosen: "+option +" - "+animal);
-  if (option =="yes" && animal =="dog") {
-      count_dog_option+=1;
-      console.log("count dog:"+ count_dog_option)
-  } else if (option =="yes" && animal=="cat")  {
-      count_cat_option+=1;
-      console.log("count cat:"+ count_cat_option)
+  console.log("func optionChosen: " + option + " - " + animal);
+  if (option === "yes" && animal === "dog") {
+      count_dog_option += 1;
+      console.log("count dog:" + count_dog_option)
+  } else if (option === "yes" && animal === "cat")  {
+      count_cat_option += 1;
+      console.log("count cat:" + count_cat_option)
   }
 
 }
 
 function question(quiz) { 
   $('#question').html(quiz.question);
+  $('#animal').val(quiz.animal);
 }
 
 function message() {
- 
-  if(count_dog_option > count_cat_option){
+  if (count_dog_option > count_cat_option){
      $('#message').html("Dog Lover");
-  }else if(count_cat_option > count_dog_option){
+  } else if(count_cat_option > count_dog_option){
      $('#message').html("Cat Lover");
-  }else if(count_cat_option == count_dog_option) {
+  } else if(count_cat_option === count_dog_option) {
      $('#message').html("Plant Lover");
   }    
 }
 
   
 function startQuestion(){
- 
   hideDivQuiz();
   showQuiz();
 }
